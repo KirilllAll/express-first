9679bfad2e03144e65be7f74bdfcfd34f0a18d32
Kirill_Aksenov <aksenovki@gmail.com>
2024-07-25 12:57:47 +1000

    com

--- api/mock.ts
+++
@@ -1,9 +0,0 @@
-export let DB = {
-    courses: [
-        { id: 1, title: 'Front-end' },
-        { id: 2, title: 'Back-end' },
-        { id: 3, title: 'Fullstack' },
-        { id: 4, title: 'QA' },
-        { id: 5, title: 'Devops' },
-    ],
-};
