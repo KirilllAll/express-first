f8c3d186be564c978980b40cd182783108b57b9d
Kirill_Aksenov <aksenovki@gmail.com>
2024-07-25 12:22:04 +1000

    commit push

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { DB } from './mock';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 let { courses } = DB;
 
 const jsonBodyMiddleware = express.json();
